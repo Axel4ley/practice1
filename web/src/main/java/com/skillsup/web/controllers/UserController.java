eb18b458b24db8cb98ac635959a35f1256f7cacb
OOP <edgargaytsuk@gmail.com>
2018-06-02 13:53:57 +0300

    02.06.2018 - 2

--- web/src/main/java/com/skillsup/web/controllers/UserController.java
+++ web/src/main/java/com/skillsup/web/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.skillsup.web.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -15,6 +17,8 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
+    private final static Logger log = LoggerFactory.getLogger(UserController.class);
+
     private final UserService userService;
 
     @Autowired
@@ -24,12 +28,14 @@ public class UserController {
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody List<UserDto> findAll() {
+        log.info("Find all users:");
         return userService.findAll();
     }
 
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public void create(@RequestBody UserDto user) {
+        log.info("Create user: {}", user);
         userService.create(user);
     }
 
@@ -38,12 +44,14 @@ public class UserController {
     @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable("id") Long id) {
+        log.info("Delete user with id {}", id);
         userService.delete(id);
     }
 
     @RequestMapping(value = "{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.ACCEPTED)
     public void update(@PathVariable("id") Long id, @RequestBody UserDto user) {
+        log.info("Update user with id {}, data: {}", id, user);
         userService.update(id, user);
     }
 }
